096b2cc30692e57137cc8e6432a0fe9665526ce5
JaganKaartik <kaartikjagan@gmail.com>
2020-09-16 22:44:33 +0530

    Prettier-Code

--- models/Dishes.js
+++ models/Dishes.js
@@ -2,26 +2,26 @@ const Sequelize = require('sequelize')
 const sequelize = require('./').sequelize
 
 const Dishes = sequelize.define(
-    'Dishes',
-    {
-        id: {
-            type: Sequelize.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        name: {
-            type: Sequelize.STRING,
-        },
-        type: {
-            type: Sequelize.STRING,
-        },
-        price: {
-            type: Sequelize.STRING,
-        },
+  'Dishes',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        timestamps: false,
-    }
+    name: {
+      type: Sequelize.STRING,
+    },
+    type: {
+      type: Sequelize.STRING,
+    },
+    price: {
+      type: Sequelize.STRING,
+    },
+  },
+  {
+    timestamps: false,
+  }
 )
 
 Dishes.sync()
--- seed.js
+++ seed.js
@@ -1,60 +1,71 @@
 const Dishes = require('./models/Dishes').Dishes
 
 const seed = () => {
-    return Dishes.bulkCreate([{
-        name: 'French Toast',
-        type: 'Bread',
-        price: '$6.00'
-    }, {
-        name: 'Penne Arabiata',
-        type: 'Pasta',
-        price: '$10.00'
-    }, {
-        name: 'Pasta Bologaniase',
-        type: 'Pasta',
-        price: '$10.00'
-    }, {
-        name: 'Hot Dog',
-        type: 'Snacks',
-        price: '$1:50'
-    }, {
-        name: 'Macaronni',
-        type: 'Pasta',
-        price: '$7.50'
+  return Dishes.bulkCreate([
+    {
+      name: 'French Toast',
+      type: 'Bread',
+      price: '$6.00',
     },
     {
-        name: 'Croissant',
-        type: 'bread',
-        price: '$0.50'
-    }, {
-        name: 'Baguette',
-        type: 'Bread',
-        price: '$2.50'
-    }, {
-        name: 'Doughnut',
-        type: 'Confections',
-        price: '$1.50'
+      name: 'Penne Arabiata',
+      type: 'Pasta',
+      price: '$10.00',
     },
     {
-        name: 'Pain Au Chocolat',
-        type: 'Bread',
-        price: '$2.00'
+      name: 'Pasta Bologaniase',
+      type: 'Pasta',
+      price: '$10.00',
     },
     {
-        name: 'Macaroons',
-        type: 'Confections',
-        price: '$2.50'
+      name: 'Hot Dog',
+      type: 'Snacks',
+      price: '$1:50',
     },
     {
-        name: 'Baguette',
-        type: 'Bread',
-        price: '$2.55'
-    }
-    ]).then(() => {
-        console.log('Seed Data Successfully Inserted')
-    }).catch((err) => {
-        console.log(err)
+      name: 'Macaronni',
+      type: 'Pasta',
+      price: '$7.50',
+    },
+    {
+      name: 'Croissant',
+      type: 'bread',
+      price: '$0.50',
+    },
+    {
+      name: 'Baguette',
+      type: 'Bread',
+      price: '$2.50',
+    },
+    {
+      name: 'Doughnut',
+      type: 'Confections',
+      price: '$1.50',
+    },
+    {
+      name: 'Pain Au Chocolat',
+      type: 'Bread',
+      price: '$2.00',
+    },
+    {
+      name: 'Macaroons',
+      type: 'Confections',
+      price: '$2.50',
+    },
+    {
+      name: 'Baguette',
+      type: 'Bread',
+      price: '$2.55',
+    },
+  ])
+    .then(() => {
+      console.log('Seed Data Successfully Inserted')
+    })
+    .catch((err) => {
+      console.log(err)
     })
 }
 
-seed().then(() => { process.exit() })
+seed().then(() => {
+  process.exit()
+})
